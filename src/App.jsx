9fa9df5ed56ccddd5ad7b40dd36e5632d7687d7a
Wajdi Hidouri <djohidouri1920@gmail.com>
2024-02-12 20:18:06 +0100

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Nav } from "./components/Nav";
 
 
 export function App() {
   return (
-    <div className="text-4xl">hello</div>
+    <div className="p-10 xl:px-24">
+      <Nav />
+    </div>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,17 @@
-
-
-export function App() {
+import NikeLogo from "../assets/nike-logo.svg?react";
+import {RxHamburgerMenu} from "react-icons/rx";
+export function Nav() {
   return (
-    <div className=""></div>
+    <nav className="flex items-center justify-between">
+      {/* Logo */}
+      <a href="">
+      <NikeLogo className="h-20 w-20"/>
+      </a>
+      {/* Burger button */}
+      <button className="p-2 rounded-lg focus:ring-2 focus:ring-gray-200">
+        <RxHamburgerMenu size={25} />
+      </button>
+    </nav>
   )
 }
 
