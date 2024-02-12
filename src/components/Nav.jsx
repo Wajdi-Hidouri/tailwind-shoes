311fdc70c958cb724afeb1860291074dd7bdcb5d
Wajdi Hidouri <djohidouri1920@gmail.com>
2024-02-12 23:14:40 +0100

    Response mobile menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,8 @@
+import { TbShoppingBag } from "react-icons/tb"; 
 import NikeLogo from "../assets/nike-logo.svg?react";
 import {RxHamburgerMenu} from "react-icons/rx";
-const Roures = [
+import { useState } from "react";
+const ROUTES = [
   "Home",
   "About",
   "Services",
@@ -8,32 +10,40 @@ const Roures = [
   "Contact",
 ]
 export function Nav() {
+  const [isMobileMenuShow , setIsMobileMenuShow]=useState(false)
   return (
     <nav className="flex flex-wrap items-center justify-between">
       {/* Logo */}
-      <a href="">
+      <a href="#">
       <NikeLogo className="h-20 w-20"/>
       </a>
       {/* Burger button */}
-      <button className="p-2 rounded-lg focus:ring-2 focus:ring-gray-200">
+      <button onClick={() => setIsMobileMenuShow(!isMobileMenuShow)} className="lg:hidden p-2 rounded-lg focus:ring-2 focus:ring-gray-200">
         <RxHamburgerMenu size={25} />
       </button>
       {/* Menu list */}
-      <div className="w-full">
-        <ul className="rounded-lg border border-gray-100 p-4 bg-gray-50 text-lg">
+      <div className={`${isMobileMenuShow === false && "hidden"
+      } w-full lg:w-auto lg:block`}>
+        <ul className="lg:space-x-8 flex flex-col lg:flex-row rounded-lg border border-gray-100 p-4 bg-gray-50 text-lg lg:bg-transparent lg:border-none">
         {ROUTES.map((route, i ) => {
           return (<li className={`cursor-pointer px-3 py-2 rounded 
           ${i === 0 
-            ? "bg-blue-500 text-white "
+            ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500 "
             : "hover:bg-gray-100"}`}
              key={route}>
               {route}
               </li>);
         })}
         </ul>
-        
-
       </div>
+      {/* Carte button */}
+      <div className="fixed bottom-4 left-4 lg:static">
+        <div className="flex-center rounded-full bg-white shadow-md h-12 w-12">
+          <TbShoppingBag />
+        </div>
+      </div>
+
+      
     </nav>
   )
 }
